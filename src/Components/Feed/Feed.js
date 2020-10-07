b7cb4ecd6b99e1b78aa268e48d14130c4d711a73
Dinesh <dineshyalla@gmail.com>
2020-10-07 00:21:48 -0700

    Added Post component

--- src/Components/Feed/Feed.js
+++ src/Components/Feed/Feed.js
@@ -3,12 +3,20 @@ import StoryReel from '../StoryReel/StoryReel';
 import '../StoryReel/StoryReel.css';
 import MessageSender from '../MessageSender/MessageSender';
 import '../MessageSender/MessageSender.css';
+import Post from '../Post/Post';
 
 const Feed = () => {
   return (
     <div className="feed">
       <StoryReel />
       <MessageSender />
+      <Post
+        profilePic=""
+        message="yoo this is the message"
+        timestamp="1601493943737"
+        imgName="imgName"
+        username="Sachin"
+      />
     </div>
   );
 };
